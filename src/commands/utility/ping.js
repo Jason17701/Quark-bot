7ffb5ac6639fdecead46e587ddb757e31d54019d
Jason17701 <jasonh17701coding@gmail.com>
2025-07-08 09:47:24 -0700

    Organize folder layout

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 
 module.exports = {
+	category: 'utility',
 	// Create a new SlashCommandBuilder instance
 	data: new SlashCommandBuilder()
 		.setName('ping')
